8f98480dbc818d69401e9cf461c59a0b22ef7d80
stefan judis <stefanjudis@gmail.com>
2020-08-31 15:29:50 +0200

    Update and Polish readme`

--- README.md
+++ README.md
@@ -3,16 +3,17 @@
 ## Contentful data
 
 <!-- CONTENTFUL_START -->
-- 〰🎋🚯 [npm install supports local modules](https://www.stefanjudis.com/today-i-learned/npm-install-supports-local-modules)
-- ✖️👇😧 [Double-click the edges of a window in macOS to expand it](https://www.stefanjudis.com/today-i-learned/double-click-the-edges-of-a-window-in-macos-to-expand-it)
-- 😋🌕🎑 [You can import Google Analytics Stats in caniuse.com](https://www.stefanjudis.com/today-i-learned/you-can-import-google-analytics-stats-in-caniuse-com)
-- 🐐😺⤴️ [Powerful history command shortcuts in bash](https://www.stefanjudis.com/today-i-learned/powerful-history-command-shortcuts-in-bash)
-- 😾☺️😖 [Commands in the Node.js REPL](https://www.stefanjudis.com/today-i-learned/commands-in-the-node-js-repl)
-- 🈚️😦🐸 [The power of cat and stdin](https://www.stefanjudis.com/today-i-learned/the-power-of-cat-and-stdin)
-- ⁉️🚓🌰 [A quick way to take pretty window screenshots on macOS](https://www.stefanjudis.com/today-i-learned/a-quick-way-to-take-pretty-window-screenshots-on-macos)
-- ♓️📷🈲 [A preceding space prevents dangerous commands from going into history](https://www.stefanjudis.com/today-i-learned/a-preceding-space-prevents-dangerous-commands-from-going-into-history)
-- 🍌💷🇪🇸 [The NPM_CONFIG_PRODUCTION flag sets the NOVE_ENV variable to production in npm scripts](https://www.stefanjudis.com/today-i-learned/the-npm_config_production-flag-sets-the-nove_env-variable-to-production-in)
-- 📆💖🐖 [Focus events include a relatedTarget property](https://www.stefanjudis.com/today-i-learned/focus-events-include-a-relatedtarget-property)
+
+- [npm install supports local modules](https://www.stefanjudis.com/today-i-learned/npm-install-supports-local-modules)
+- [Double-click the edges of a window in macOS to expand it](https://www.stefanjudis.com/today-i-learned/double-click-the-edges-of-a-window-in-macos-to-expand-it)
+- [You can import Google Analytics Stats in caniuse.com](https://www.stefanjudis.com/today-i-learned/you-can-import-google-analytics-stats-in-caniuse-com)
+- [Powerful history command shortcuts in bash](https://www.stefanjudis.com/today-i-learned/powerful-history-command-shortcuts-in-bash)
+- [Commands in the Node.js REPL](https://www.stefanjudis.com/today-i-learned/commands-in-the-node-js-repl)
+- [The power of cat and stdin](https://www.stefanjudis.com/today-i-learned/the-power-of-cat-and-stdin)
+- [A quick way to take pretty window screenshots on macOS](https://www.stefanjudis.com/today-i-learned/a-quick-way-to-take-pretty-window-screenshots-on-macos)
+- [A preceding space prevents dangerous commands from going into history](https://www.stefanjudis.com/today-i-learned/a-preceding-space-prevents-dangerous-commands-from-going-into-history)
+- [The NPM_CONFIG_PRODUCTION flag sets the NOVE_ENV variable to production in npm scripts](https://www.stefanjudis.com/today-i-learned/the-npm_config_production-flag-sets-the-nove_env-variable-to-production-in)
+- [Focus events include a relatedTarget property](https://www.stefanjudis.com/today-i-learned/focus-events-include-a-relatedtarget-property)
 <!-- CONTENTFUL_END -->
 
 ## Code of conduct
--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const { readFile, writeFile } = require('fs').promises;
-const { join } = require('path');
 const got = require('got');
-const randomEmoji = require('random-emoji');
 
-const query = `
-query tilPostCollection {
-  tilPostCollection(limit: 10) {
-    total
-    items {
-      title
-      slug
-    }
-  }
-}
-`;
+// use 'join' to include "/" or "\" in file paths depending on operating system
+const { join } = require('path');
+const README_PATH = join(__dirname, 'README.md');
 
 const { SPACE_ID, AUTH_TOKEN } = process.env;
 
+/**
+ * Fetch data from Contentful and return GraphQL data
+ *
+ * @return  {Promise<Array>}  [return description]
+ */
 async function fetchTilPosts() {
-  const { body } = await got(
-    `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${AUTH_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query }),
-    }
-  );
-
   try {
+    const { body } = await got(
+      `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${AUTH_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+            query tilPostCollection {
+              tilPostCollection(limit: 10) {
+                total
+                items {
+                  title
+                  slug
+                }
+              }
+            }`,
+        }),
+      }
+    );
+
     const posts = JSON.parse(body).data.tilPostCollection.items;
     return posts;
   } catch (error) {
@@ -39,32 +45,45 @@ async function fetchTilPosts() {
   }
 }
 
-(async () => {
+/**
+ * Update and replace the Contentful placeholders
+ * of the passed readme with some real API data
+ *
+ * @param   {string}  readme  String of read README.md
+ *
+ * @return  {Promise<string>}          Updated README.md
+ */
+async function getUpdatedReadme(readme) {
   const posts = await fetchTilPosts();
-
-  const readme = await readFile(join(__dirname, 'README.md'), 'utf8');
   const contentfulDataRegex = /<!-- CONTENTFUL_START.*?CONTENTFUL_END -->/gs;
 
-  const newReadme = readme.replace(
+  return readme.replace(
     contentfulDataRegex,
-    `<!-- CONTENTFUL_START -->
-${posts
-  .map(
-    ({ title, slug }) =>
-      `- ${randomEmoji
-        .random({ count: 3 })
-        .map(({ character }) => character)
-        .join(
-          ''
-        )} [${title}](https://www.stefanjudis.com/today-i-learned/${slug})`
-  )
-  .join('\n')}
-<!-- CONTENTFUL_END -->`
+    [
+      '<!-- CONTENTFUL_START -->',
+      ...posts.map(
+        ({ title, slug }) =>
+          `- [${title}](https://www.stefanjudis.com/today-i-learned/${slug})`
+      ),
+      '<!-- CONTENTFUL_END -->',
+    ].join('\n')
   );
+}
 
-  console.log(newReadme);
+/**
+ * @return  {Promise}
+ */
+async function main() {
+  // read README.md
+  const readme = await readFile(README_PATH, 'utf8');
 
-  await writeFile(join(__dirname, 'README.md'), newReadme, 'utf8');
-  console.log(`You've written the readme`);
-  console.log(newReadme);
-})();
+  // do something with the readme
+  const updatedReadme = await getUpdatedReadme(readme);
+
+  // write README.md
+  await writeFile(README_PATH, updatedReadme, 'utf8');
+  console.log(`New readme written!`);
+  console.log(updatedReadme);
+}
+
+main();
